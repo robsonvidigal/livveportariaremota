05614bab9cd6e9521d0d0ebd68eb63a2e64fbe75
robsonvidigal <robsonvidigal@gmail.com>
2022-07-03 13:00:19 -0300

    v. 0,06 - Titulo: Geral
    v. 0,06	-	Titulo: Geral	-	Data	:	03/07/2022	-	Por	:	Robson Vidigal	-	Atualização	:	Add Logo Century, add data, pasta de credito e estrut. Demais.

--- livve/javascript/relogio.js
+++ livve/javascript/relogio.js
@@ -9,7 +9,7 @@
         var segundo = date.getSeconds();
         var session = "AM";
 
-        //Condição da utilização de AM PM
+        //Condição da utilização de saudação - Bom dia/Boa Tarde e Boa noite.
              if(hora==0){
                 hora=12;
             }
@@ -17,13 +17,21 @@
              if(hora>12){
                 hora = hora - 12;
                 session = "PM"
-             }
+            }
 
     hora = (hora<10) ? "0" + hora : hora;
     minuto = (minuto<10) ? "0" + minuto : minuto;
     segundo = (segundo<10) ? "0" + segundo : segundo;
 
-    var time = hora + ":" + minuto + ":" + segundo + " " + session;
+        //Estrutura da Data   
+
+            //Dias da semana
+            diaNome = new Array ("domingo", "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado");
+            mesNome = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro");
+            agora = new Date ();
+
+
+    var time = "hora: " + hora + ":" + minuto + ":" + segundo + " " + session + " / " + diaNome[agora.getDay()] + ", " + agora.getDate() + " de " + mesNome[agora.getMonth()] + " de " + agora.getFullYear();
     document.getElementById("hms").innerHTML = time;
     document.getElementById("hms").textContent = time;
 
@@ -32,8 +40,3 @@
     }
 
     showTime();
-
-
-//Créditos: Feature Code
-//font: https://github.com/tluis9/relogioWeb
-//      https://www.youtube.com/watch?v=O2t1brzQ6NE
---
+++ livve/teste_php.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<title>Boletim - Livve Portária</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+<body>
+	
+	<header id="header">
+		
+		<div class="logo">
+			<img src="img/logo_pq.png" alt="Livve Portaria Remota">
+		</div>
+		<nav id="nav">
+			<button aria-label="Abrir Menu" id="btn-mobile" aria-haspopup="true" aria-controls="menu" aria-expanded="false">Menu
+        <sapn id="hamburger"></sapn>
+      </button>
+			<ul id="menu" role="menu">
+				<li><a href="">INICIO</a></li>
+				<li><a href="">N.A. OPEN</a></li>
+				<li><a href="">N.A. CONSULTA</a></li>
+				<li><a href="">ADMIM</a></li>
+			</ul>
+		</nav>
+	</header>
+	
+	<div id="conteudogeral">
+		<div class="fundotitulo">
+        	
+			<h2>BOLETIM OPERACIONAL</h2>
+		
+			<div class="logo2">
+				<img src="img/logo_pq_branco.png" alt="Livve Portaria Remota">
+			</div> 
+		
+		</div>
+        
+		<div id="conteudo">
+            
+			<div id="hms" class="hora" onload="showTime()"></div>
+        
+		
+		
+		</div>
+        
+    </div>	
+	
+	<script src="javascript/funcoes.js"></script>
+	<script src="javascript/relogio.js"></script>
+</body>
+</html>
